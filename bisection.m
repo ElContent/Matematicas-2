ed0fb1bba6d769ffe49cea380dee827cc2e24ffb
ElContent <61986615+ElContent@users.noreply.github.com>
2020-04-17 13:04:29 +0200

    Update bisection.m

--- bisection.m
+++ bisection.m
@@ -1,7 +1,33 @@
-function [outputArg1,outputArg2] = bisection(inputArg1,inputArg2)
+function [outputArg1,outputArg2] = bisection(f,a,b,epi,delta,n)
 %bisection Summary of this function goes here
 %   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+
+i = 0;
+salida = 0;
+while salida == 0
+    i = i + 1;
+    c = (a+b)/2;
+    if (f(a)*f(c)) < 0
+        b = c;
+    else
+        a = c;
+    end
+    
+    % Condiciones de salida
+    if (abs(b-a)/2) < delta
+        salida = 1;
+    end
+    if f(c) < epi
+        salida = 1;
+    end
+    if i == n
+        salida = 1;
+    end
+end
+
+
+
+outputArg1 = f;
+outputArg2 = a;
 end
 
