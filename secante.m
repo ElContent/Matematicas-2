f5dbb1f7aafc7ab796311840eff8bf0f1aadd2bb
ifranferrandez <61840533+ifranferrandez@users.noreply.github.com>
2020-04-20 16:35:55 +0200

    Add files via upload

---
+++ secante.m
@@ -0,0 +1,48 @@
+function secante(fun, A_var, B_var, tolerancia, e, N)
+syms x;
+inc = 1;
+
+loop_bool = true;
+
+    while (loop_bool == true)
+        if(abs(subs(fun, A_var)) > abs(subs(fun, B_var)))
+            aux = A_var;
+            A_var = B_var;
+            B_var = aux;
+        end
+
+        h = (subs(fun, A_var)*(B_var-A_var))/(subs(fun,B_var)-subs(fun,A_var));
+        c = A_var-h;
+        
+        i(inc, 1) = double(inc);
+        A(inc, 1) = double(A_var);
+        B(inc, 1) = double(B_var);
+        C(inc, 1) = double(c);
+        H(inc, 1) = double(h);
+        funA(inc, 1) = double(subs(fun, A_var));
+        funB(inc, 1) = double(subs(fun, B_var));
+        funC(inc, 1) = double(subs(fun, c));
+
+        
+        B_var = c;       
+   
+        T = table(i, A, B, C, H, funA, funB, funC);
+
+        if inc >= N 
+            loop_bool = false;
+            disp(T);
+            disp("Número de iteraciones máximo alcanzado.");
+        else if abs(subs(fun,c)) <= e
+                loop_bool = false;
+                disp(T);
+                disp("El valor absoluto de funC es menor al error e.");
+            else if abs(h) <= tolerancia
+                    loop_bool = false;
+                    disp(T);
+                    disp("El valor absoluto de h es menor a la tolerancia.");
+                end
+            end 
+        end  
+        inc = inc + 1;
+    end
+end
