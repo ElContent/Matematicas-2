c093f59788341fb9c1c272b8b31261b89da705ac
ElContent <61986615+ElContent@users.noreply.github.com>
2020-04-20 03:59:15 +0200

    Newton
    Añado método de Newton
    No sé si lo de que la solución converge o diverge está bien o es al contrario.

--- newton.m
+++ newton.m
@@ -1,21 +1,44 @@
-function [x,iter]=newton(x0,f,df)
+function newton(fun, A_var)
 % Algoritmo Newton-Raphson
-N = 100; eps = 1.e-5; %Defino max. Numero. iteraciones y error
-maxval = 10000.0; % Defino valor para divergencia
-x=x0;
-while (N>0)
-    xn = x-f(x)/df(x);%Función de Newton
-    if abs(f(xn))<eps
-       x=xn;iter=100-N;
-       return;
+% Calculamos la derivada:
+der = diff(fun);
+
+% Definimos el error:
+error = 1.e-5;
+
+% Definimos el valor para la divergencia:
+maxVal = 10000.0;
+
+% Preparamos el bucle:
+i = 0;
+while_out = false;
+while (i <= 100 && while_out == false)
+    i = i + 1;
+    h_var = subs(fun, A_var) / subs(der, A_var);
+    C_var = A_var - h_var;
+    
+    % Condición de parada:
+    if (abs(h_var) >= maxVal || abs(subs(fun, C_var)) < error)
+        while_out = true;
     end
-    if abs(f(x))>maxval
-       disp(['# de Iteraciones = ',num2str(iter)]);
-       error('La Solucion Diverge');
-       break
-    end
-    N = N - 1;
-    x = xn;
+    
+    % Preparamos la tabla:
+    inc(i, 1) = i;
+    A(i, 1) = double(A_var);
+    h(i, 1) = double(h_var);
+    C(i, 1) = double(C_var);
+    funInC(i, 1) = double(subs(fun, C_var));
+    T = table(inc, A, h, C, funInC);
+    
+    A_var = C_var;
 end
-error('No Converge');
+
+disp(T);
+fprintf("Total iteraciones: %d \n", i);
+
+if abs(h_var) >= maxVal
+    fprintf('La solución DIVERGE \n');
+else
+    fprintf('La solución CONVERGE \n');
+end
 % Fin de la Funcion
