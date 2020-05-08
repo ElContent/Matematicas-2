e012cfb9ee921e23e30129bb6967e156980eb0e4
ElContent <61986615+ElContent@users.noreply.github.com>
2020-05-08 12:05:04 +0200

    Update menuInterpolacion.m

--- menuInterpolacion.m
+++ menuInterpolacion.m
@@ -2,4 +2,8 @@
 clear
 clc
 
-fprintf("Todavía no se ha implementado \n");
+% Cabecera:
+fprintf("### Calculadora d'interpolació per distints mètodes ### \n");
+fprintf("Enginyeria Informàtica - Universitat d'Alacant - Grup en Valencià \n\n");
+
+fprintf("Encara no s'ha implementat \n");
