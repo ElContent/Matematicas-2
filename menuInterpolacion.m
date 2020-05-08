0c9966f5a7c5b199f20bbf88e309de3750caadac
ElContent <61986615+ElContent@users.noreply.github.com>
2020-05-08 15:11:36 +0200

    Update menuInterpolacion.m

--- menuInterpolacion.m
+++ menuInterpolacion.m
@@ -6,4 +6,28 @@ clc
 fprintf("### Calculadora d'interpolació per distints mètodes ### \n");
 fprintf("Enginyeria Informàtica - Universitat d'Alacant - Grup en Valencià \n\n");
 
-fprintf("Encara no s'ha implementat \n");
+selection_bool = true;
+while selection_bool
+    Referencia = ["1."; "2."];
+    Contenido = ["Lagrange"; "La Grange"];
+    qstn = table(Referencia, Contenido);
+    disp(qstn);
+    go = input("Introduzca la referencia del contenido a ejecutar: ");
+    fprintf("\n");
+    
+    switch go
+        case 1
+            % Lagrange:
+            fprintf("Encara no s'ha implementat \n");
+            selection_bool = false;
+        
+        case 2
+            % La Grange:
+            fprintf("Encara no s'ha implementat \n");
+            selection_bool = false;
+            
+        otherwise
+            % Error de selección:
+            fprintf("Error: Selección fuera de rango \n\n");
+    end
+end
