719c7ccccb163c160d06274e0200cb4e1056b725
ElContent <61986615+ElContent@users.noreply.github.com>
2020-05-09 20:35:48 +0200

    Update de todos los menús
    Actualización de los menús

--- menuInterpolacion.m
+++ menuInterpolacion.m
@@ -8,8 +8,9 @@ fprintf("Enginyeria Informàtica - Universitat d'Alacant - Grup en Valencià \n\n"
 
 selection_bool = true;
 while selection_bool
-    Referencia = ["1."; "2."];
-    Metode = ["Lagrange"; "La Grange"];
+    Referencia = ["1."; "2."; "3."; "4."; "5."; "6."; "7."];
+    Metode = ["Lagrange"; "Tablas de Neville"; "Diferencias Divididas";...
+        "Hermite"; "Splines"; "Newton"; "La Grange"];
     qstn = table(Referencia, Metode);
     disp(qstn);
     go = input("Introduzca la referencia del contenido a ejecutar: ");
@@ -20,8 +21,33 @@ while selection_bool
             % Lagrange:
             fprintf("Encara no s'ha implementat \n");
             selection_bool = false;
-        
+            
         case 2
+            % Tablas de Neville
+            fprintf("Encara no s'ha implementat \n");
+            selection_bool = false;
+            
+        case 3
+            % Diferencias Divididas
+            fprintf("Encara no s'ha implementat \n");
+            selection_bool = false;
+            
+        case 4
+            % Hermite
+            fprintf("Encara no s'ha implementat \n");
+            selection_bool = false;
+            
+        case 5
+            % Splines
+            fprintf("Encara no s'ha implementat \n");
+            selection_bool = false;
+            
+        case 6
+            % Newton
+            fprintf("Encara no s'ha implementat \n");
+            selection_bool = false;
+        
+        case 7
             % La Grange:
             web('https://www.youtube.com/watch?v=Vppbdf-qtGU');
             selection_bool = false;
