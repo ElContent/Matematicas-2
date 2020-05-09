cb05c9aff8741e3c9ee55dcdb5c8c939d408f220
josuejx <64470097+josuejx@users.noreply.github.com>
2020-05-09 22:41:22 +0200

    Update iNewton.m

--- Interpolacion/iNewton.m
+++ Interpolacion/iNewton.m
@@ -1,15 +1,17 @@
 % Interpolacion de Newton
-function [p, yi, C] = iNewton(x, y, xi)
+function [C, p, yi] = iNewton(x, y, xi)
 % Inicializa variables
 n = length(x); %n elementos del vector
 C = zeros(n);  %Inicilizo una matriz
 C(:,1) = y(:);  % Las columnas de la matriz será el vector y
+
 % Obtener la tabla de diferencias
 for j = 2:n
     for i = 1:n-j+1
         C(i,j) = (C(i+1, j-1)-C(i,j))/(x(i+j-1)-x(i));
     end
 end
+
 % Calcular el dato interpolado
 xt = 1;
 yi = C(1,1);
@@ -17,6 +19,7 @@ for j=1:n-1
     xt = xt.*(xi-x(j));
     yi = yi+C(1,j+i)*xt;
 end
+
 % Construir el polinomio
 p = num2str(C(1,1));
 xx = x*-1;
