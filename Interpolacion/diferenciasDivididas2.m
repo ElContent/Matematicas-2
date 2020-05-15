f20d4469c80cc82e8864b4928341d722f634a6d8
ElContent <61986615+ElContent@users.noreply.github.com>
2020-05-15 11:13:28 +0200

    Update diferenciasDivididas2.m

--- Interpolacion/diferenciasDivididas2.m
+++ Interpolacion/diferenciasDivididas2.m
@@ -4,13 +4,27 @@ function diferenciasDivididas2(fun, V)
 % array es el vector de los valores con los que trabajaremos.
 % imprimirTabla es un dato booleano que usaremos por si queremos imprimir
 % la tabla por pantalla o por si no queremos.
-
+syms x;
 if not(isa(fun,'function_handle'))
     fun = matlabFunction(fun);
 end
 
-syms x;
+N = 2*length(V) - 1;
 
+% Nombre de la cabecera:
+for i = 1:length(V)
+    if i >= 2
+        coma = coma + ",";
+    else
+        coma = "";
+    end %endif
+end %endfor
+nombreCabecera = "[" + coma + "]";
+
+i = 1;
+% Creando la tabla:
+tabla.cabecera = "Xi";
+tabla.col1 = 
 
 
 end
