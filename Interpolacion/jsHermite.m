a35302ebdaa63f822e776b878cc07719ca23e210
ElContent <61986615+ElContent@users.noreply.github.com>
2020-05-22 03:17:00 +0200

    Update jsHermite.m

--- Interpolacion/jsHermite.m
+++ Interpolacion/jsHermite.m
@@ -1,4 +1,4 @@
-function jsHermite(N)
+function jsHermite(V, N)
 % N -> Cantidad de polinomios que vamos a imprimir
 if N > 0 
     if N >= 1
@@ -6,15 +6,12 @@ if N > 0
     end
     
 % Plot the first five Hermite polynomials
-       x = -3:0.05:3;
        n = 0:N;
-       y = hermite(n,x);
-       plot(x, y, 'LineWidth', 2)
-       xlabel('X')
-       ylabel('H_n(X)')
+       y = hermite(n,V);
+       plot(V, y, 'LineWidth', 2)
        %title('Hermite polynomials for N = 0 to 4')
-       set(gca,'YLim',[-25 25])
-       %legend(strcat('N=',num2str(n')),'Location','SouthEast')
+       set(gca,'YLim',[-10 10])
+       legend(strcat('N=',num2str(n')),'Location','SouthEast')
 end
 end
 
